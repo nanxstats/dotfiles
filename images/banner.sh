e79a7a97136f04cda874739f90644a2299bd477d
Nan Xiao <me@nanx.me>
2024-10-06 04:16:51 -0400

    Adjust font size

--- images/banner.sh
+++ images/banner.sh
@@ -3,11 +3,11 @@
 magick -size 2048x734 \
   -define gradient:angle=330 \gradient:#03448c-#17ffc6 \
   -gravity center \
-  -pointsize 105 \
+  -pointsize 90 \
   -font 'Cascadia-Code-SemiBold' \
   -fill white \
   -annotate +0-50 'nanxstats/dotfiles' \
-  -pointsize 28 \
+  -pointsize 40 \
   -font 'Cascadia-Code-SemiBold' \
   -annotate +0+100 'Homebrew · VS Code' \
   png:- | pngquant - --force --output images/banner.png
